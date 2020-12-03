570c1ed4bb5c836f53a460343ec9db02f4bb929f
Andres Carrascal Verona <andres.carrascal@bpt.com.co>
2020-12-02 23:00:47 -0500

    Se configura la base de datos en postgrestSQL

--- src/main/java/co/edu/udea/basededatos/entity/Departamento.java
+++ src/main/java/co/edu/udea/basededatos/entity/Departamento.java
@@ -3,25 +3,31 @@ package co.edu.udea.basededatos.entity;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "pais")
-public class Pais {
+@Table(name = "departamento")
+public class Departamento {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "indicativo", nullable = false)
-    private String indicativo;
-
     @Column(name = "nombre", nullable = false, length = 100)
     private String nombre;
 
-    @Column(name = "nombre_corto", length = 3)
+    @Column(name = "nombre_corto", length = 5)
     private String nombreCorto;
 
     @Column(name = "habilitado", nullable = false)
     private Boolean habilitado;
+
+    @Column(name = "fk_pais", nullable = false)
+    private Long fkPais;
+
+    // relationships
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="fk_pais", insertable = false, updatable = false)
+    private Pais pais;
 
     public Long getId() {
         return id;
@@ -29,14 +35,6 @@ public class Pais {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getIndicativo() {
-        return indicativo;
-    }
-
-    public void setIndicativo(String indicativo) {
-        this.indicativo = indicativo;
     }
 
     public String getNombre() {
@@ -61,5 +59,21 @@ public class Pais {
 
     public void setHabilitado(Boolean habilitado) {
         this.habilitado = habilitado;
+    }
+
+    public Long getFkPais() {
+        return fkPais;
+    }
+
+    public void setFkPais(Long fkPais) {
+        this.fkPais = fkPais;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
     }
 }
